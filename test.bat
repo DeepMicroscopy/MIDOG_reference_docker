5e4cf08d3917f793562a5424d21e2fcd0f1d11b5
Marc Aubreville <marc.aubreville@fau.de>
2021-08-04 14:39:23 +0200

    Fixed filename in batch file

--- test.bat
+++ test.bat
@@ -10,12 +10,12 @@ docker run --rm^
 
 docker run --rm^
  -v mitosisdetection-output:/output/^
- python:3.7-slim cat /output/mitotic_figures.json | python -m json.tool
+ python:3.7-slim cat /output/mitotic-figures.json | python -m json.tool
 
 docker run --rm^
  -v mitosisdetection-output:/output/^
  -v %~dp0\test\:/input/^
- python:3.7-slim python -c "import json, sys; f1 = json.load(open('/output/mitotic_figures.json')); f2 = json.load(open('/input/expected_output.json')); sys.exit(f1 != f2);"
+ python:3.7-slim python -c "import json, sys; f1 = json.load(open('/output/mitotic-figures.json')); f2 = json.load(open('/input/expected_output.json')); sys.exit(f1 != f2);"
 
 if %ERRORLEVEL% == 0 (
 	echo "Tests successfully passed..."
